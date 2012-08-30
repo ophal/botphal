f7141a1a8c4c84b2e555f4af8ebbe3b5bd7fa71e
Mitchell Monahan <soccermitchy@gmail.com>
2012-08-29 21:25:36 -0400

    Added rcserver.lua, not called yet by anything. Currently borked.

---
+++ rcserver.lua
@@ -0,0 +1,34 @@
+function rcserver()
+   serverActive = true
+   rcsocket=require "socket"
+   rcserver=rcsocket.bind("*",80808)
+   coroutine.yield()
+   while serverActive do
+        rcserver:settimeout(1)
+		rcclient=rcserver:accept()
+		if rcclient then
+		    coroutine.yield()
+	        remoteip=rcclient:getpeername()
+	        if remoteip == "94.249.210.90" then
+	            exec,err=loadstring(data)
+	        	if err then
+	        	    rcclient:send(err)
+                elseif not exec then
+	        	    rcclient:send("Unexpected error.")
+		        else
+		        	returned=""
+			        s,r=pcall(exec)
+			        if r==nil then
+			        	returned=returned.."[Nothing returned!]"
+        			elseif not type(r)== "string" then
+		        		returned=returned.."[Script returned a "..type(r).."]"
+        			else
+		        		returned=returned..r
+			        end
+        		    rcclient:send(returned)
+		        end
+        		coroutine.yield()
+			end
+    	end
+    end
+end
