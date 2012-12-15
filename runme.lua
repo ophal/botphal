7f51b06eb817bc772af350bb19b4cbe58624dec8
Mitchell Monahan <soccermitchy@gmail.com>
2012-12-15 09:05:13 -0500

    completed what https://github.com/ophal/botphal/issues/2 asks...

--- runme.lua
+++ runme.lua
@@ -1,18 +1,8 @@
 dofile("irc/init.lua")
 dofile("functions.lua")
 if exists("config.lua") then dofile("config.lua") else error "I don't think you got the config.lua (Or I was not able to find it)" end
-if not config then error "Did you just create a blank config.lua?!?!?! We have it in the git repo." end
-if config.die then error "You might want to edit config.lua..." end
-if not config.serverPort then error "Edit the config file" end
-if not config.server then error "Edit the config.lua." end
-if not config.autorun then error "Edit the config.lua." end
-if not config.channels then error "Edit the config.lua." end
---if not config.enabledCommands then error "Edit the config.lua." end
-if not config.nick then error "Edit the config.lua." end
-if not config.username then error "Edit the config.lua" end
-if not config.realname then error "Edit the config.lua" end
-if not config.factoids then error "Edit the config.lua"
-elseif config.factoids.enabled == "true" then
+if not (config or config.serverPort or config.server or config.autorun config.channels or config.enabledCommands or config.nick or config.username or config.realname or config.factoids) then error "Edit config.lua" end
+if config.factoids.enabled == "true" then
     if not config.factoids.db then
 	   error "You are missing a config.factoids.db config option. (Block: factoids)"
 	end
