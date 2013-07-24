e1384f71621c1dbb0b188f42d4ee15226f372f10
Mitchell Monahan <soccermitchy@gmail.com>
2013-07-23 20:53:23 -0400

    halp

--- plugins/admin.lua
+++ plugins/admin.lua
@@ -1,6 +1,20 @@
-function reload(usr,chan,nick,args)
+function reload(usr,chan,msg,args)
 	dofile"load.lua"
 	if chan:sub(1,1)=="#" then target="chan" else target="usr" end
 	return target,string.format("%s, done!",usr.nick)
 end
+function lua(usr,chan,msg,args)
+	if chan:sub(1,1)=="#" then target="chan" else target="usr" end
+	local code=table.concat(args," ")
+	local func,err=loadstring(code)
+	if err then return target,usr.nick..": "..err end
+	local asdf,ret=pcall(func) asdf=nil
+	ret = tostring(ret) or "No output"
+	return target,usr.nick..": "..ret
+end
+function shutdown(usr,chan,msg,args)
+	irc:send("QUIT :My owner told me to quit!")
+end
+addcommand("shutdown",shutdown,"owner")
+addcommand("lua",config.cmdchar,"trusted")
 addcommand("reload",reload)
